c91e1b2cc8d3f1e91a0d110f57998acd918666f7
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-01-31 08:48:31 -0600

    get method

--- genericExample.java
+++ genericExample.java
@@ -7,5 +7,11 @@ public class genericExample{
     System.out.println(list.size());
     list.add(0, "Welborn");
     System.out.println(list);
+    System.out.println(list.contains ("Kulis"));
+    System.out.println(list.contains ("Lauren"));
+    String firstName = list.get(1);
+    System.out.println(firstName);
+    String lastName = list.get(2);
+
   }
 }
