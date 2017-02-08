20c7f6115af30ca0e5b60bfdd5431790414161f7
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-08 08:46:45 -0600

    inclass 2/8

---
+++ edu/jalc/inclass/SodaBottleApp.java
@@ -0,0 +1,8 @@
+package edu.jalc.inclass.bottle;
+public class SodaBottleApp {
+
+	public static void main(String... args){
+		SodaBottle sodaBottle = new SodaBottle();
+		System.out.println("Capacity: " + sodaBottle.getCapacity());
+	}
+}
