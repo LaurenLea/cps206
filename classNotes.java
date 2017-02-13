67ae912ab49770dbd7c61f65f9e704bbc787356f
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-13 08:50:51 -0600

    class 2/13

---
+++ classNotes.java
@@ -0,0 +1,14 @@
+# Private Classes
+  ```
+public class dog{
+  private DogCollar dogCollar = new DogCollar();
+  private class DogCollar{
+    }
+  }
+  ```
+private inner class
+
+# Static
+when you create something static you create one instance of it
+
+public final static is constant
